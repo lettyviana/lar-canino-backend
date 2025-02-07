141b76f43183568932e62914509e4287df2a927c
lettyviana <letisviana@gmail.com>
2025-02-06 22:05:34 -0300

    refactor: modify connection to Database

--- lib/dbConfig/connectToDataBase.ts
+++ lib/dbConfig/connectToDataBase.ts
@@ -2,29 +2,46 @@ import mongoose from "mongoose";
 
 const { MONGODB_CONNECTION_STRING } = process.env;
 
+if (!MONGODB_CONNECTION_STRING) {
+  throw new Error(
+    "A variável de ambiente MONGODB_CONNECTION_STRING não foi definida no arquivo .env."
+  );
+}
+
+interface IMongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+const globalAny: any = global;
+
+let cached: IMongooseCache = globalAny.mongoose;
+
+if (!cached) {
+  cached = globalAny.mongoose = { conn: null, promise: null };
+}
+
 const connectToDatabase = async () => {
-  if (!MONGODB_CONNECTION_STRING) {
-    return new Response(
-      JSON.stringify({
-        error:
-          "A variável de ambiente de configuração do banco não foi informada.",
-      }),
-      { status: 500 }
-    );
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  try {
-    if (mongoose.connection.readyState === 0) {
-      mongoose.connection.on("connected", () =>
-      console.log("Banco de dados conectado.")
-      );
-      await mongoose.connect(MONGODB_CONNECTION_STRING);
-    }
-  } catch (error: any) {
-    mongoose.connection.on("error", (error) =>
-        console.log(`Erro ao conectar ao banco de dados: ${error}`)
-      );
+  if (!cached.promise) {
+    cached.promise = mongoose
+    .connect(MONGODB_CONNECTION_STRING)
+    .then((mongoose) => {
+      console.log("Banco de dados conectado.");
+      return mongoose;
+    })
+    .catch((error) => {
+      console.error("Erro ao conectar ao banco de dados:", error);
+      throw error;
+    });
   }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
 
 export default connectToDatabase;
